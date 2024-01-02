88448777d53c9d6b282579c8658d94990188ad33
thstmddns <thstmddns@naver.com>
2024-01-02 17:30:13 +0900

    CORS 설정, 라이브러리 설치

--- soso/main.py
+++ soso/main.py
@@ -1,2 +1,16 @@
 from fastapi import FastAPI
-from 
+from pydantic import BaseModel
+from utils import *
+from fastapi.middleware.cors import CORSMiddleware
+
+#  iniitialize fast api
+app = FastAPI
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처 허용. 실제 배포 시에는 필요한 도메인만 허용해야 합니다.
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
+tokenizer, model, device = initializeModels() 
