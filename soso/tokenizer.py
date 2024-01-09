1a46b06743143af6f7c7b799074ff49629e62853
thstmddns <thstmddns@naver.com>
2024-01-09 15:31:31 +0900

    tokenizer.py파일 만들기

---
+++ soso/tokenizer.py
@@ -0,0 +1,11 @@
+import json
+from transformers import AutoTokenizer
+
+with open('./soso/data/dacon_data.json', 'r', encoding='UTF8') as f:
+    json_data = json.load(f)
+
+print(json_data)
+
+tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
+read_jsond_data = json_data
+tokens = tokenizer.tokenize(read_jsond_data)
